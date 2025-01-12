c47180332c1f4b19b6956b45c6c9dbfd85d7a7f6
Suhira30 <suhirabalarajan@gmail.com>
2025-01-12 23:29:36 +0530

    00

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,10 +19,13 @@ export default {
 			// => @media (min-width: 1440px) { ... }
 		  },
 		  fontSize: {
-			sm: ['14px', '20px'],
-			base: ['16px', '24px'],
-			lg: ['20px', '28px'],
-			xl: ['24px', '32px'],
+			sm: '0.8rem',
+			base: '1rem',
+			xl: '1.25rem',
+			'2xl': '1.563rem',
+			'3xl': '1.953rem',
+			'4xl': '2.441rem',
+			'5xl': '3.052rem',
 		  }
 	},
 
